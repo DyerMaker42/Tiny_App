5bcb296edbe2e542714174659c61e28f61e2ef2d
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-28 22:12:43 +0000

    tidied express server

--- express_server.js
+++ express_server.js
@@ -18,10 +18,10 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get('/urls.json',(req,res)=>{
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req,res) => {
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
