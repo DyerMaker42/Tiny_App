d4482406188c333273644f9b41d8b85cb6da4300
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-28 21:06:20 +0000

    added base logic to server confirming listener is set

--- express_server.js
+++ express_server.js
@@ -6,3 +6,12 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/", (req, res) => {
+  res.send("hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
