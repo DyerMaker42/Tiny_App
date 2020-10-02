edac483e653820e3eaf3ab0e4f02212f4c5a9c55
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-02 02:01:31 +0000

    refactored generaterandomstring

--- helpers.js
+++ helpers.js
@@ -1,13 +1,12 @@
 //app functions================================================
+
 //generates 6 digit string (ie Uoh87x)
 const generateRandomString = () => {
   // give set of characters to choose
   const lettersNumbers = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
-  //generate random number
   let randNumber = () => {
     return Math.floor(Math.random() * 63);
   };
-  //loop through six digits
   let array = [];
   for (let i = 0; i < 6; i++) {
     array.push(lettersNumbers.charAt(randNumber()));
@@ -18,7 +17,6 @@ const generateRandomString = () => {
 ///**helper function that retrieves user id bu email,///
 const getUserbyEmail = (userEmail, database) => {
   for (let id in database) {
-    //console.log(key,"key")
     if (database[id].email === userEmail) {
       return database[id];
     }
